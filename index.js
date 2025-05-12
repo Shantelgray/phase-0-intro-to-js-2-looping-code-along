0a847e64e2b17c3bb080310a1bf5917c7f1c260d
Shantel <Shantelgray24@gmail.com>
2025-05-11 22:55:23 -0400

    adding the for loop and while loop

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  let messages = [];
+for (let i = 0; i < names.length; i++) {
+messages[i] = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  
+  }
+  return messages;
+}
+console.logwriteCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
